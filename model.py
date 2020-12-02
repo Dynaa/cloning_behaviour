a306a82eeeaa0aa301e7d9f722e5e515929ad725
Dynaa <ma.mittet@gmail.com>
2020-12-02 09:55:39 +0900

    Add model test

--- model.py
+++ model.py
@@ -45,8 +45,8 @@ def simple_model():
 
 X_train, y_train = read_data('./data/')
 
-#model = simple_model()
-#model.compile(loss='mse', optmizer='adam')
-#model.fit(x_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
-#model.save('model.h5')
+model = simple_model()
+model.compile(loss='mse', optmizer='adam')
+model.fit(x_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.save('model.h5')
 
