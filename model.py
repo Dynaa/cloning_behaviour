400e653fdd5a4a57df6ae66a2e7f111e146dfde0
root <ma.mittet@gmail.com>
2020-12-02 07:39:31 +0000

    Update from udacity

--- model.py
+++ model.py
@@ -38,7 +38,7 @@ def read_data(data_directory):
 
 def simple_model():
 	model = Sequential()
-	model.add(Lambda(lambda x: (x/ 255.0) - 0.5, input_shape=(160,320,3)))
+	model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 	model.add(Flatten())
 	model.add(Dense(1))
 
@@ -49,6 +49,6 @@ X_train, y_train = read_data('./data/')
 
 model = simple_model()
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
 model.save('model.h5')
 
