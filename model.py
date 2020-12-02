d92c78f05f257f027e82ffd4ef6931be32c84e34
Dynaa <ma.mittet@gmail.com>
2020-12-02 09:58:56 +0900

    Typo

--- model.py
+++ model.py
@@ -49,6 +49,6 @@ X_train, y_train = read_data('./data/')
 
 model = simple_model()
 model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
 model.save('model.h5')
 
