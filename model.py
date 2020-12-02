124aad7d05b32c739283967bcf2c93d8eb265840
Dynaa <ma.mittet@gmail.com>
2020-12-02 09:33:41 +0900

    Loading data and first simple model

---
+++ model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import csv
+import cv2
+
+
+def read_data(data_directory): 
+	lines = []
+    with open(data_directory+'driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader, None)  # skip the headers #skip header
+        for line in reader:
+            lines.append(line)
+
+    images = []
+    measurements = []
+    for line in lines:
+        source_path = line[0]
+        filename = source_path.split('/')[-1]
+        current_path = data_directory+'IMG/' + filename
+        #print(current_path)
+        image = cv2.imread(current_path)
+        images.append(image)
+        measurement = float(line[3])
+        measurements.append(measurement)
+
+    X_train = np.array(images)
+    y_train = np.array(measurements)
+
+	return X_train, y_train
+
+def simple_model():
+	model = Sequential()
+	model.add(Lambda(lambda x: (x/ 255.0) - 0.5, input_shape=(160,320,3)))
+	model.add(Flatten())
+	model.add(Dense(1))
+
+	return model 
+
+
+X_train, y_train = read_data("./data")
+
+model = simple_model()
+model.compile(loss='mse', optmizer='adam')
+model.fit(x_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.save('model.h5')
+
